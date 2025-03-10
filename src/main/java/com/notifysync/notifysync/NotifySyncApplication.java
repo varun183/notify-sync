1e3a21855aad36476c808c27cea18585a2eb38bb
S Varun <varunshanker1718@gmail.com>
2025-03-10 17:40:34 +0530

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/notifysync/notifysync/NotifySyncApplication.java
@@ -0,0 +1,13 @@
+package com.notifysync.notifysync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifySyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifysync/notifysync/NotifySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifysync.notifysync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
